64a809f4b83347e3fb6a8d63efd4d85748999f86
maxogod <maxi18wars@gmail.com>
2024-02-21 17:10:26 -0300

    fix poner bien los archivos de los tests

--- tests.py
+++ tests.py
@@ -16,7 +16,7 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(res, len(ejecucion))
 
     def test_7_subconjuntos(self):
-        file = './archivos_prueba/5.txt'
+        file = './archivos_prueba/7.txt'
         res = 2
 
         conjunto, subconjuntos = obtener_conjunto_y_subconjuntos(file)
@@ -25,7 +25,7 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(res, len(ejecucion))
 
     def test_10_pocos_subconjuntos(self):
-        file = './archivos_prueba/5.txt'
+        file = './archivos_prueba/10_pocos.txt'
         res = 3
 
         conjunto, subconjuntos = obtener_conjunto_y_subconjuntos(file)
@@ -34,7 +34,7 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(res, len(ejecucion))
 
     def test_10_todos_subconjuntos(self):
-        file = './archivos_prueba/5.txt'
+        file = './archivos_prueba/10_todos.txt'
         res = 10
 
         conjunto, subconjuntos = obtener_conjunto_y_subconjuntos(file)
@@ -43,7 +43,7 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(res, len(ejecucion))
 
     def test_10_varios_subconjuntos(self):
-        file = './archivos_prueba/5.txt'
+        file = './archivos_prueba/10_varios.txt'
         res = 6
 
         conjunto, subconjuntos = obtener_conjunto_y_subconjuntos(file)
@@ -52,7 +52,7 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(res, len(ejecucion))
 
     def test_15_subconjuntos(self):
-        file = './archivos_prueba/5.txt'
+        file = './archivos_prueba/15.txt'
         res = 4
 
         conjunto, subconjuntos = obtener_conjunto_y_subconjuntos(file)
@@ -61,7 +61,7 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(res, len(ejecucion))
 
     def test_20_subconjuntos(self):
-        file = './archivos_prueba/5.txt'
+        file = './archivos_prueba/20.txt'
         res = 5
 
         conjunto, subconjuntos = obtener_conjunto_y_subconjuntos(file)
@@ -70,7 +70,7 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(res, len(ejecucion))
 
     def test_50_subconjuntos(self):
-        file = './archivos_prueba/5.txt'
+        file = './archivos_prueba/50.txt'
         res = 6
 
         conjunto, subconjuntos = obtener_conjunto_y_subconjuntos(file)
@@ -79,7 +79,7 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(res, len(ejecucion))
 
     def test_75_subconjuntos(self):
-        file = './archivos_prueba/5.txt'
+        file = './archivos_prueba/75.txt'
         res = 8
 
         conjunto, subconjuntos = obtener_conjunto_y_subconjuntos(file)
@@ -88,7 +88,7 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(res, len(ejecucion))
 
     def test_100_subconjuntos(self):
-        file = './archivos_prueba/5.txt'
+        file = './archivos_prueba/100.txt'
         res = 9
 
         conjunto, subconjuntos = obtener_conjunto_y_subconjuntos(file)
@@ -97,7 +97,7 @@ class UnitTests(unittest.TestCase):
         self.assertEqual(res, len(ejecucion))
 
     def test_200_subconjuntos(self):
-        file = './archivos_prueba/5.txt'
+        file = './archivos_prueba/200.txt'
         res = 9
 
         conjunto, subconjuntos = obtener_conjunto_y_subconjuntos(file)
