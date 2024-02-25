5b420cd1be920b86e8dfda5348d6605395719ac1
Juani-tech <jperezdichiazza@gmail.com>
2024-02-25 18:19:12 -0300

    optimization to weight computation

--- algoritmo_aproximado.py
+++ algoritmo_aproximado.py
@@ -6,27 +6,25 @@ def calcular_pesos(subconjuntos):
     pesos = {}
     for subconjunto in subconjuntos:
         for jugador in subconjunto:
-            pesos[jugador] = pesos.get(jugador, 0) + 1
-
-    for jugador in pesos.keys():
-        for subconjunto in subconjuntos:
-            if jugador in subconjunto:
-                pesos[jugador] = 1 / pesos[jugador] * len(subconjunto)        
+            pesos[jugador] = pesos.get(jugador, 0) + 1 / len(subconjunto)
     return pesos
 
 def hitting_set_greedy(A, B):
     a = list(A)
-    x = list(B)
-    pesos = calcular_pesos(x)
+    pesos = calcular_pesos(B)
     a.sort(key=lambda jugador: pesos.get(jugador, 0))
     hitting_set = []
-    while len(x) > 0:
+    while len(B) > 0:
         hitting_set.append(a[0])
-        subsets = [s for s in subsets if a[0] not in s]        
+        B = [s for s in B if a[0] not in s]        
         a = []
-        for subset in subsets:
-            for jugador in subset:
+        for _set in B:
+            for jugador in _set:
                 if jugador not in a:
                     a.append(jugador)
-                    
     return hitting_set
+
+
+
+
+
