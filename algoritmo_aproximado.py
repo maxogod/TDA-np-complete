77140fd5040d000ba408fad087791fd1925bde00
maxogod <maxi18wars@gmail.com>
2024-02-27 17:04:44 -0300

    cleanup

--- algoritmo_aproximado.py
+++ algoritmo_aproximado.py
@@ -6,21 +6,21 @@ from utils.utils import obtener_conjunto_y_subconjuntos
 # Esto es lo que se tiene que buscar minimizar en cada paso segun el K&T, 11.3
 def calcular_coeficiente(subconjuntos):
     pesos = {}
+    suma_largos_subconjuntos = 0
     for subconjunto in subconjuntos:
         for jugador in subconjunto:
-            if jugador not in pesos:
-                pesos[jugador] = {'frecuencia': 0, 'largo_total': 0}
-            pesos[jugador]['frecuencia'] += 1
-            pesos[jugador]['largo_total'] += len(subconjunto)
+            pesos[jugador] = pesos.get(jugador, 0) + 1
+        suma_largos_subconjuntos += len(subconjunto)
+        
         
     for jugador in pesos.keys():
-        pesos[jugador] = 1 / (pesos[jugador]['frecuencia'] * pesos[jugador]['largo_total'])
+        pesos[jugador] = 1 / (pesos[jugador] * suma_largos_subconjuntos)
    
     return pesos
 
 def hitting_set_greedy(A, B):
     hitting_set = []
-    while len(B) > 0:
+    while B:
         pesos = calcular_coeficiente(B)
 
         a = [jugador for jugador in pesos.keys()]
@@ -40,4 +40,3 @@ if __name__ == "__main__":
     A, B = obtener_conjunto_y_subconjuntos(args[1])
     ejecucion = hitting_set_greedy(A, B)
     print(f"Jugadores Totales: {len(ejecucion)}\nLista de Jugadores: {ejecucion}")
-
