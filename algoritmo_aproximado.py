60a59a3525cccecbe61f03bdd90d158429cb5849
Juani-tech <jperezdichiazza@gmail.com>
2024-02-27 14:39:59 -0300

    cambio de nombre calcular_pesos por calcular_coeficientes

--- algoritmo_aproximado.py
+++ algoritmo_aproximado.py
@@ -4,7 +4,7 @@ import sys
 from utils.utils import obtener_conjunto_y_subconjuntos
 
 # Esto es lo que se tiene que buscar minimizar en cada paso segun el K&T, 11.3
-def calcular_pesos(subconjuntos):
+def calcular_coeficiente(subconjuntos):
     pesos = {}
     for subconjunto in subconjuntos:
         for jugador in subconjunto:
@@ -21,14 +21,14 @@ def calcular_pesos(subconjuntos):
 def hitting_set_greedy(A, B):
     a = list(A)
     hitting_set = []
-    pesos = calcular_pesos(B)
+    pesos = calcular_coeficiente(B)
     a.sort(key=lambda jugador: pesos.get(jugador, 0))
     while len(B) > 0:
         hitting_set.append(a[0])
         
         B = [s for s in B if a[0] not in s]        
         pesos.pop(a[0], None)
-        pesos = calcular_pesos(B) # Esto ya itera sobre los subsets, por lo que si algo no existe en B, no se agrega a a
+        pesos = calcular_coeficiente(B) # Esto ya itera sobre los subsets, por lo que si algo no existe en B, no se agrega a a
         a = [jugador for jugador in pesos.keys()]
         a.sort(key=lambda jugador: pesos.get(jugador, 0))
         
