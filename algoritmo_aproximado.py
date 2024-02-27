e4ea1c99e5316843413aa95dd8925629d54bb513
maxogod <maxi18wars@gmail.com>
2024-02-27 15:11:25 -0300

    ejecutable

--- algoritmo_aproximado.py
+++ algoritmo_aproximado.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import sys
+from utils.utils import obtener_conjunto_y_subconjuntos
+
 # Esto es lo que se tiene que buscar minimizar en cada paso segun el K&T, 11.3
 def calcular_pesos(subconjuntos):
     pesos = {}
@@ -30,3 +33,11 @@ def hitting_set_greedy(A, B):
         a.sort(key=lambda jugador: pesos.get(jugador, 0))
         
     return hitting_set
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    A, B = obtener_conjunto_y_subconjuntos(args[1])
+    ejecucion = hitting_set_greedy(A, B)
+    print(f"Jugadores Totales: {len(ejecucion)}\nLista de Jugadores: {ejecucion}")
+
